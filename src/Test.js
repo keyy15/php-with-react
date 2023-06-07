7c41128f486deb28746bdc49589b7feb5390fb40
keyy15 <smeyhem01@gmail.com>
2023-06-07 16:55:14 +0700

    proejct

---
+++ src/Test.js
@@ -0,0 +1,13 @@
+import './index.css';
+
+function Test(){
+    return(
+        <div className="test">
+            <h2>
+                Hello Ah Bek
+            </h2>
+        </div>
+    );
+}
+
+export default Test; 
